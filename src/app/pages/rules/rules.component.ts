ac30464f572953ba6948d574bc9134c366abfc5e
humberto <humbertoconstantino73@gmail.com>
2024-09-12 22:25:44 -0300

    Finalizando tela de regras (rules)

--- src/app/pages/rules/rules.component.ts
+++ src/app/pages/rules/rules.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { TopicComponent } from '../../components/topic/topic.component';
 
 @Component({
   selector: 'app-rules',
   standalone: true,
-  imports: [],
+  imports: [TopicComponent],
   templateUrl: './rules.component.html',
-  styleUrl: './rules.component.scss'
+  styleUrl: './rules.component.scss',
 })
 export class RulesComponent {
-
+  rules = [
+    'Carinho e atenção não pode faltar;',
+    'Cuide de pulgas, carrapatos, entre outros parasitas;',
+    'Leve ao veterinário regularmente;',
+    'Mantenha as vacinas em dia, é importante para ele(a) e para você.',
+    'A castração é importante para prevenir doenças e a procriação.',
+  ];
 }
