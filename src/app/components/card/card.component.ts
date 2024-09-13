090bf443c923c869b3c588fea40b10bd2c837ae0
humberto <humbertoconstantino73@gmail.com>
2024-09-13 13:18:49 -0300

    Ajutes função

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,17 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { Animals } from '../../models/animals';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
   @Input() animal: Animals = {};
+  bgColor = '#E5E5E5';
+  imgHeart = 'https://i.imgur.com/HgEHAJP.png';
 
-  ngOnInit() {
-    console.log(this.animal);
+  //Precisei fazer assim pra economizar tempo. A maneira ideal deveria ser pelo scss.
+
+  changeHeart() {
+    const defaultColor = '#E5E5E5';
+    const activeColor = '#bf80ff33';
+    const activeImage = 'https://i.imgur.com/O55VYIy.png';
+    const defaultImage = 'https://i.imgur.com/HgEHAJP.png';
+
+    if (this.bgColor === defaultColor) {
+      this.bgColor = activeColor;
+      this.imgHeart = activeImage;
+    } else {
+      this.bgColor = defaultColor;
+      this.imgHeart = defaultImage;
+    }
   }
 }
